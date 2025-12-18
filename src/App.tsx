0b6ae2174707bc0d5ce8fed64683037d1788e059
Harry7986 <emarh.harry.code@gmail.com>
2025-12-18 11:52:09 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import ThemeToggle from "./components/ThemeToggle";
+import { ThemeProvider } from "./components/ThemeProvider";
 
-function App() {
-
+export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <ThemeProvider>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white flex flex-col items-center justify-center gap-6">
+        <h1 className="text-4xl font-bold">Dark Mode Test</h1>
+        <ThemeToggle />
+      </div>
+    </ThemeProvider>
+  );
 }
-
-export default App
